19ca4502c2000d710b2efb4e46d43c5c2332d658
seedahmed <seedo564@gmail.com>
2024-08-02 17:13:00 +0300

    first commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-
+app.use(express.static("public"));
 app.use("/api/admin", adminRoutes);
 app.use("/api/config", configRoute);
 app.use("/api/customer", custumerRote);
